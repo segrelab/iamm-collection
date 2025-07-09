741159eb7e546a26a5695f3dd8915931d050a8a9
Helen Scott <helengracescott@gmail.com>
2025-07-08 21:39:17 -0400

    Edit the first instance of the contig ID only

--- shorten_serr_contig_names.sh
+++ shorten_serr_contig_names.sh
@@ -11,6 +11,6 @@ OUTPUT_FILE="/projectnb/hfsp/IAMM_reference_files/fasta/2526164535-custom-contig
 echo "Processing ${INPUT_FILE}..."
 
 # Run sed on the input file and redirect the standard output to the new file
-sed 's/_draft_12-05-13.1$//' "$INPUT_FILE" > "$OUTPUT_FILE"
+sed 's/_draft_12-05-13.1//' "$INPUT_FILE" > "$OUTPUT_FILE"
 
 echo "Done. Modified FASTA saved to ${OUTPUT_FILE}"
